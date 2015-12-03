3a755a8771b86c2024e04af8afbcf37db59f9aa7
Hyman <1604723751@qq.com>
2015-12-03 21:36:29 +0800

    Add test method to the current code.

--- MovieRental/MovieRental/Program.cs
+++ MovieRental/MovieRental/Program.cs
@@ -10,6 +10,20 @@ namespace MovieRental
     {
         public static void Main(string[] args)
         {
+            Movie movie1 = new Movie("007", Movie.NEW_RELEASE);
+            Movie Movie2 = new Movie("Cat and Mouse", Movie.CHILDRENS);
+            Movie movie3 = new Movie("The Lost", Movie.REGULAR);
+            Rental rental1 = new Rental(movie1, 10);
+            Rental rental2 = new Rental(Movie2, 2);
+            Rental rental3 = new Rental(movie3, 3);
+
+            Customer customer = new Customer("Tom");
+            customer.AddRental(rental1);
+            customer.AddRental(rental2);
+            customer.AddRental(rental3);
+
+            Console.Write(customer.Statement());
+            Console.Read();
         }
     }
 }
